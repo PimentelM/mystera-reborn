9cbba9372333830d7624c51fb12d716c0088720b
Arthur Melo <arthrmlo@gmail.com>
2019-07-04 18:05:37 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# 🤖 Mystera Legacy
+
+## General info
+
+Sample accounts for testing:
+
+- Usernames:
+
+```text
+Mlo
+DonoDosBot
+PsadeloDoDev
+BotTop
+```
+
+- Password (all accounts):
+
+```text
+batata
+```
+
+## Website
+
+- [Mystera Legacy](https://www.mysteralegacy.com/)
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HTMLPlugin()
+  ]
+};
