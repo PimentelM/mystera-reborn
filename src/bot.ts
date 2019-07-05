78ee1ec2ac1503114cd6a6c9d2c92115fb3c397b
Luiz Melo <zberyt@gmail.com>
2019-07-04 23:40:59 -0300

    Added Simple Parser

--- src/bot.ts
+++ src/bot.ts
@@ -1,9 +1,10 @@
-export default class bot{
+export default class bot {
     public socket_is_open: boolean | undefined;
     public ws: WebSocket | undefined;
     public nick: string;
     public debug_mode: boolean;
-    constructor(public login: string,public password: string){
+    public msg_array: any;
+    constructor(public login: string, public password: string) {
         this.debug_mode = true;
         this.nick = login;
         this.login = btoa(login);
@@ -14,7 +15,7 @@ export default class bot{
         this.create_socket();
     }
 
-    create_socket(){
+    create_socket() {
         const new_socket = new WebSocket('wss://br.mysteralegacy.com/');
         new_socket.onopen = () => {
             this.debug('WebSocket open and ready to use.');
@@ -24,28 +25,44 @@ export default class bot{
         }
     }
 
-    connect(){
-        if(this.ws != undefined){
+    connect() {
+        if (this.ws != undefined) {
             this.ws.send('{"type":"client","ver":"4.9.1","mobile":false,"agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"}');
             this.ws.send(`{"type":"login","user":"${this.login}","pass":"${this.password}"}`);
-            this.parse_messages();
+            this.treat_socket_events();
         }
     }
 
-    debug(msg: string){
-        if(this.debug_mode == true){
+    debug(msg: string) {
+        if (this.debug_mode == true) {
             console.log(msg);
         }
     }
 
-    parse_messages(){
-        if(this.ws != undefined){
+    treat_socket_events() {
+        if (this.ws != undefined) {
+            this.ws.onerror = err => {
+                console.log(err);
+            }
+            this.ws.onclose = cls => {
+                console.log(cls);
+            }
             this.ws.onmessage = msg => {
-                let clean_msg = msg.data.replace(/\\/g, '');
-                console.log(clean_msg);
+                this.parse_msg(msg);
             }
         }
     }
 
+    parse_msg(msg: any){
+        let msg_clear = msg.data.replace(/\\/g, '');
+        try{
+        let msg_json = JSON.parse(msg_clear);
+        console.log(msg_json);
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
 }
+
 
