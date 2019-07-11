8008b587b1a36dfcb52cdf982a209199b82b0d14
Mateus Pimentel <mateus.pimentel.w@gmail.com>
2019-07-11 01:41:30 -0300

    Fixed bug in Heal on fountain

--- src/bot/states/misc/HealOnFountain.ts
+++ src/bot/states/misc/HealOnFountain.ts
@@ -4,27 +4,27 @@ import {Player} from "../../../game/player/Player";
 import {TilePoint} from "../../../game/Types";
 
 export interface HealOnFountainState {
-    minHealth : number,
+    minHealth: number,
 
-    originalMinHealth? : number
-    fountainTile? : TilePoint
+    originalMinHealth?: number
+    fountainTile?: TilePoint
 
 }
 
-export class HealOnFountain extends StateDefinition{
+export class HealOnFountain extends StateDefinition {
     public state: HealOnFountainState;
 
     readonly defaultParams: HealOnFountainState = {
-        minHealth : 30
+        minHealth: 30
     };
 
     async isReached(game): Promise<boolean> {
         // Player health is already ok
-        if(game.player.status.hpppc > this.state.minHealth) return true;
+        if (game.player.status.hpppc > this.state.minHealth) return true;
 
         this.state.fountainTile = await this.findFountain(game);
         // There is no fountain nearbly
-        if(!this.state.fountainTile) return true;
+        if (!this.state.fountainTile) return true;
 
         return false;
     }
@@ -33,7 +33,7 @@ export class HealOnFountain extends StateDefinition{
         let tilePoint = this.state.fountainTile;
 
         // If player is adjacent to fountain;
-        if(game.player.isAdjacentTo(tilePoint)) {
+        if (game.player.isAdjacentTo(tilePoint)) {
             await game.player.lookAt(tilePoint);
             game.player.action();
             this.state.minHealth = this.state.originalMinHealth;
@@ -41,16 +41,18 @@ export class HealOnFountain extends StateDefinition{
         }
         // If not, walk in the direction of item.
         else {
-            this.state.originalMinHealth = this.state.minHealth;
-            this.state.minHealth += 10;
+            if (!this.state.originalMinHealth || this.state.originalMinHealth == this.state.minHealth) {
+                this.state.originalMinHealth = this.state.minHealth;
+                this.state.minHealth += 10;
+            }
             game.player.walkAdjacentTo(tilePoint);
         }
 
         return true;
     }
 
-    async findFountain(game : Game){
+    async findFountain(game: Game) {
         let tiles = game.map.findTilesWithItem("Healing Fountain");
-        return await game.player.nearestReachablePoint(tiles,true);
+        return await game.player.nearestReachablePoint(tiles, true);
     }
 }
