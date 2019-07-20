40f084a32aac11fe116660cc81c2f7999cabe4d1
Mateus Pimentel <mateus.pimentel.w@gmail.com>
2019-07-20 10:45:15 -0300

    Ressending attack packet

--- src/bot/states/targeting/TargetCreature.ts
+++ src/bot/states/targeting/TargetCreature.ts
@@ -5,44 +5,57 @@ import {CreatureFilter} from "../../../game/Creatures";
 import {Mob} from "../../../game/Types";
 
 
-
 export interface TargetCreatureState {
-    filters : CreatureFilter[],
-    retarget : boolean,
-    range : number
+    filters: CreatureFilter[],
+    retarget: boolean,
+    range: number
 
-    bestTarget? : Mob,
+    bestTarget?: Mob,
     cooldown: number;
 
-    lastRetarget? : number
+    lastRetarget?: number
+    lastResend?: number
 
 }
 
 // Currently, retargeting is an expensive feature computationally speaking, so we need to examine it later to improve it's performance.
 
-export class TargetCreature extends StateDefinition{
+export class TargetCreature extends StateDefinition {
     state: TargetCreatureState;
     game: Game;
 
     readonly defaultParams: TargetCreatureState = {
-        filters : [""], retarget : true, cooldown: 2000, range : 15
+        filters: [""], retarget: true, cooldown: 2000, range: 15
+    };
+
+    resendAttackPacket = () => {
+        if (new Date().valueOf() - this.state.lastResend >= this.state.cooldown) {
+            this.game.send({type: "t", t: this.game.player.getTarget().id});
+            this.state.lastResend = new Date().valueOf();
+        }
     };
 
     async isReached(): Promise<boolean> {
         let currentTarget = this.game.player.getTarget();
+
+
         // If already has target
-        if(currentTarget) {
+        if (currentTarget) {
+
+            this.resendAttackPacket();
+
+
             // If bot is not configured to retarget;
-            if(!this.state.retarget) return true;
+            if (!this.state.retarget) return true;
 
             // If retarget is in cooldown
-            if(new Date().valueOf() - this.state.lastRetarget < this.state.cooldown) return true;
+            if (new Date().valueOf() - this.state.lastRetarget < this.state.cooldown) return true;
 
 
             this.state.bestTarget = await this.getReachableCreature();
 
             // This means that the player is attacking some creature that is not in the filter list;
-            if(!!this.state.bestTarget) return true;
+            if (!!this.state.bestTarget) return true;
 
             // If player already has the best target;
             //if (this.state.bestTarget.id === currentTarget.id) return true;
@@ -56,7 +69,7 @@ export class TargetCreature extends StateDefinition{
 
         this.state.bestTarget = await this.getReachableCreature();
         // If there are no creatures that player can target on the screen;
-        if(!this.state.bestTarget) {
+        if (!this.state.bestTarget) {
             return true;
         }
 
@@ -69,15 +82,15 @@ export class TargetCreature extends StateDefinition{
         return true;
     }
 
-    private async getReachableCreature() : Promise<Mob> {
+    private async getReachableCreature(): Promise<Mob> {
         let creatures;
 
-        for (let filter of this.state.filters){
-            creatures = this.game.creatures.findCreatures(filter,this.state.range);
-            if(creatures.length != 0) break;
+        for (let filter of this.state.filters) {
+            creatures = this.game.creatures.findCreatures(filter, this.state.range);
+            if (creatures.length != 0) break;
         }
 
-        return await this.game.player.nearestReachablePoint(creatures,true);
+        return await this.game.player.nearestReachablePoint(creatures, true);
     }
 
 
