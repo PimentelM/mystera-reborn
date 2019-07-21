7de17b0a2ef956b1933f3b0cc64a53fb6c176770
Mateus Pimentel <mateus.pimentel.w@gmail.com>
2019-07-21 00:58:30 -0300

    Updated some npm deps

--- webpack.config.js
+++ webpack.config.js
@@ -22,16 +22,19 @@ let servers = ["ust", "usw", "use", "eu", "br",  "ldn", "use2", "usw2", "sea", "
 let mystera = "http://www.mysteralegacy.com/";
 let isDev = true;
 
+
+let srcPath = 'src/client';
+
 module.exports = {
     entry: {
         polyfill: '@babel/polyfill',
-        main: helpers.root('src/client'),
+        main: helpers.root(srcPath),
     },
     resolve: {
         extensions: ['.js', '.vue', '.ts', '.tsx'],
         alias: {
             'vue$': isDev ? 'vue/dist/vue.runtime.js' : 'vue/dist/vue.runtime.min.js',
-            '@': helpers.root('src')
+            '@': helpers.root(srcPath)
         }
     },
     devtool: "cheap-module-eval-source-map",
@@ -40,18 +43,18 @@ module.exports = {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
+                use: ["ts-loader"],
                 exclude: /node_modules/
             },
             {
                 test: /\.vue$/,
                 loader: 'vue-loader',
-                include: [helpers.root('src')]
+                include: [helpers.root(srcPath)]
             },
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
-                include: [helpers.root('src')]
+                include: [helpers.root(srcPath)]
             },
             {
                 test: /\.css$/,
