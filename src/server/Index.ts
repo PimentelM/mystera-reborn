4ee6d22b9072caac2b2da73e56ff27c86dcbf86d
Mateus Pimentel <mateus.pimentel.w@gmail.com>
2024-02-18 20:59:52 -0300

    chore: disable db connection

--- src/server/Index.ts
+++ src/server/Index.ts
@@ -1,10 +1,9 @@
-import express from 'express';
-import {SetupWsProxy} from "./setup/http-server/WsProxy";
-import {SetupHttpProxy} from "./setup/http-server/HttpProxy";
-import {SetupHttpServer} from "./setup/http-server/HttpServer";
-import "./setup/Database";
+import express from "express";
+import { SetupWsProxy } from "./setup/http-server/WsProxy";
+import { SetupHttpProxy } from "./setup/http-server/HttpProxy";
+import { SetupHttpServer } from "./setup/http-server/HttpServer";
+//import "./setup/Database";
 
 let app = express();
-
 
 SetupHttpServer(app);
